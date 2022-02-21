5cd1cca577d5084649246671df9d9d0fb078350c
TreciaKS <tksekgalo@gmail.com>
2022-02-21 13:52:39 +0000

    Finally!! Got the project working!

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // grab and store ids 
 let displayEmojies = document.getElementById("display-emojis")
-let displayJokes = document.getElementById("display-joke")
 let welcomeText = document.getElementById("welcome-text")
 let generateBtn = document.getElementById("generate-btn")
 
@@ -9,10 +8,34 @@ function generateEmojies() {
     let randomEmojies = ["🤣🤣🤣", "😅😅😅", "🤭🤭🤭", "😋😋😋", "😂😂😂", "😎😎😎", "😛😛😛", "😜😜😜", "🤪🤪🤪"]
     let myEmoji = randomEmojies[Math.floor(Math.random() * randomEmojies.length)]
     displayEmojies.textContent = myEmoji
-}
-function generateJoke() {
-    welcomeText.textContent = "Funny isn't it? 😉"
-    generateBtn.style.backgroundColor = 'red'
 
-    generateEmojies()
+    // to change btn to red and adds emojies to welcome text
+    welcomeText.textContent = `Funny isn't it? 😋` 
+    generateBtn.style.backgroundColor = 'red'
+}
+
+function generateJoke() {
+    fetch("https://jokes-by-api-ninjas.p.rapidapi.com/v1/jokes?limit=30", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "jokes-by-api-ninjas.p.rapidapi.com",
+		"x-rapidapi-key": "00cdce758dmshf568644be6eb228p14b320jsn22e767e76c0c"
+	}
+})
+    .then(res => res.json())
+    .then(data => {
+        const myJoke = data
+        let text = ""
+        for (let item of myJoke) {
+            text = `
+                <p>${item.joke}</p>
+            `
+        }
+        document.getElementById("display-joke").innerHTML = text
+    })
+    .catch(err => {
+        console.error(err);
+    }) 
+
+generateEmojies() 
 }
