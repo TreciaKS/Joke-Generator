b4fa48675ba9121aa93e241d9a6092c04e0678c1
TreciaKS <tksekgalo@gmail.com>
2022-02-21 12:26:44 +0000

    random emojies array done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-''
+// grab and store ids 
+let displayEmojies = document.getElementById("display-emojis")
+let displayJokes = document.getElementById("display-joke")
+let welcomeText = document.getElementById("welcome-text")
+let generateBtn = document.getElementById("generate-btn")
+
+// generate random emojies after joke is generated (clicked)
+function generateEmojies() {
+    let randomEmojies = ["🤣🤣🤣", "😅😅😅", "🤭🤭🤭", "😋😋😋", "😂😂😂", "😎😎😎", "😛😛😛", "😜😜😜", "🤪🤪🤪"]
+    let myEmoji = randomEmojies[Math.floor(Math.random() * randomEmojies.length)]
+    displayEmojies.textContent = myEmoji
+}
+function generateJoke() {
+    welcomeText.textContent = "Funny isn't it? 😉"
+    generateBtn.style.backgroundColor = 'red'
+
+    generateEmojies()
+}
